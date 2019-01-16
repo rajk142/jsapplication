2dbcf258bdf395568f6408d13302988f67c15a25
rajk142 <46594746+rajk142@users.noreply.github.com>
2019-01-16 16:37:18 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,22 +1,52 @@
 // JavaScript source code
-
-function addItem(){
 var divAll,divInr,lstItem,delItem;
-divAll = document.querySelectorAll(".list-emp");
-divInr = document.createElement("div");
-lstItem = document.createElement("span");
-delItem = document.createElement("span");
 
-divInr.className = "list-item";
 
-lstItem.className = "Name";
-delItem.className = "del-item";
+window.onclick = function(){
+	divAll = document.querySelector(".list-emp");
+	
+	divAll.addEventListener("click",function(event){
+		delItem = document.querySelectorAll(".list-item");
+		console.log(event.target.className);
+		if (event.target.className == "del-item")
+		{
+			event.target.parentElement.remove();  
+		}
+	})
+	//divAll.forEach(function(ele,index,array){
+//		var del = ele.querySelector(".del-item")
+	//	console.log(del);
+		//del.addEventListener("click",function(event){
+			//del.parentElement.remove();
+			//})
+	//},false)
+	
 
-divInr.append(lstItem);
-divInr.append(delItem);
-divAll[0].append(divInr);
 
-lstItem.innerText = document.getElementById("add-name").value;
-delItem.innerText = "Remove";
+
 }
+
+	function addItem()
+	{
+	
+		event.preventDefault();
+
+
+		divInr = document.createElement("div");
+		lstItem = document.createElement("span");
+		delItem = document.createElement("span");
+
+		divInr.className = "list-item";
+
+		lstItem.className = "Name";
+		delItem.className = "del-item";
+
+		divInr.append(lstItem);
+		divInr.append(delItem);
+		divAll.append(divInr);
+
+		lstItem.innerText = document.getElementById("add-name").value;
+		delItem.innerText = "Remove";
+	}
+
 
